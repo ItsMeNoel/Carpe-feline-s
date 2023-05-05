58d582ac186cc8bf8d51f0d51f7ccfda4d866b34
ItsMeNoel <noel.garedal@elev.ga.ntig.se>
2023-05-05 14:38:07 +0200

    img test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const petsData = [
       födelseår: "temp",
       info: "jkasldö asldk jaölsdkf",
       birthYear: 2017,
-      photo: src="/Images/Yasuria.jpg"
+      photo: "/Images/Yasuria.jpg"
     },
     {
       namn: "Tori",
