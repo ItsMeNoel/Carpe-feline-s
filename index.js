b4b4656b6384a30ac10d13779ba2bad36d4fbb66
ItsMeNoel <noel.garedal@elev.ga.ntig.se>
2023-05-07 21:42:19 +0200

    test

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const petsData = [
       namn: "Tori",
       födelseår: "Maj 2020",
       info: "Tori ( Dagdrivarn Lias Vittoria) är en fd avelshona som fick en tarminvagination när hennes kull var knappt fem veckor gammal. Hon är svartsmoke och vit. Hon har blivit kastrerad pga att jag inte vill utsätta henne för en kull ungar till.",
-      photo: "images/Tori2.jpg"
+      photo: "Images/Tori2.jpg"
     },
     {
       namn: "Zebrina",
       födelseår: "22 februari 2022",
       info: "Piplisa (Carpe Feline's Zebrina), Piplisa är svartsilvertabby och kullsyster till Yassi och har blivit kvar från den kullen. Piplisa är numera kastrerad och kommer att flytta till eget hem. Hon pratar alltid, därav sitt smeknamn. ",
-      photo: "images/Zebrina.jpg"
+      photo: "Images/Zebrina.jpg"
     },
     {
         namn: "Oscarii",
         födelseår: "10 juli 2021",
         info: "Oreo (Carpe Feline's Oscarii) är en svart herre med lite vitt. Han bor i foderhem och blir bortskämd. Han har träffat två honor hittills och ska få träffa några till, sedan blir det pension för honom. ",
-        photo: "images/Oscarii.jpg"
+        photo: "Images/Oscarii.jpg"
       },
      
   ];
